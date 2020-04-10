666868e38ad08a1e1048c98007d9fe46a2a6a5cb
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 12:41:06 +0530

    Print the first 3 cells in sequence which given by user

--- src/stagesOfTicTacToeGame/SecondStage.java
+++ src/stagesOfTicTacToeGame/SecondStage.java
@@ -7,5 +7,7 @@ public class SecondStage {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter cells:");
         String cells = scanner.next();
+        System.out.println("---------");
+        System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
     }
 }
