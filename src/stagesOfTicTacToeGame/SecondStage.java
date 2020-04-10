ece4d0119c5e04977b88c1adccf87ca7a54f6b7c
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 12:44:35 +0530

    Print the last 3 cells

--- src/stagesOfTicTacToeGame/SecondStage.java
+++ src/stagesOfTicTacToeGame/SecondStage.java
@@ -10,5 +10,6 @@ public class SecondStage {
         System.out.println("---------");
         System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
         System.out.println("| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |");
+        System.out.println("| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |");
     }
 }
