4185a0ca8824aeed3f7c6047066bf4f76c590595
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 15:05:28 +0530

    Create Tic-Tac-Toe format and the select variable is '_' then replace into spaces of particular indexes

--- src/stagesOfTicTacToeGame/FourthStage.java
+++ src/stagesOfTicTacToeGame/FourthStage.java
@@ -10,6 +10,20 @@ public class FourthStage {
         System.out.println("---------");
         char[][] newCells = {{cells[0], cells[1], cells[2]}, {cells[3], cells[4], cells[5]}, {cells[6], cells[7], cells[8]}};
         System.out.print("| ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (newCells[i][j] == '_') {
+                    newCells[i][j] = ' ';
+                }
+                System.out.print(newCells[i][j] + " ");
+            }
+            if (i != 2) {
+                System.out.print("|\n| ");
+            } else {
+                System.out.println("|");
+            }
+        }
+        System.out.println("---------");
 
     }
 }
