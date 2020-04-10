66e7cf7437dfcf6e6f371a6f15a6f65597d2a189
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 12:35:22 +0530

    Input all 8 cells in one line

--- src/stagesOfTicTacToeGame/SecondStage.java
+++ src/stagesOfTicTacToeGame/SecondStage.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SecondStage {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        System.out.println("Enter cells:");
+        String cells = scanner.next();
     }
 }
