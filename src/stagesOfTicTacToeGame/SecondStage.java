17ecfe4ff77f4f122bd0f26e9c6266be2b3f13db
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 12:18:21 +0530

    Create a Scanner class object to input by user

--- src/stagesOfTicTacToeGame/SecondStage.java
+++ src/stagesOfTicTacToeGame/SecondStage.java
@@ -1,7 +1,11 @@
 package stagesOfTicTacToeGame;
 
+import java.util.Scanner;
+
 public class SecondStage {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 }
