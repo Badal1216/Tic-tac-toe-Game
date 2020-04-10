ce0879b9c04d9d01a1dcecfd6a8d9ac8ad502918
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 14:45:18 +0530

    create a 2-D array of character type name as newCells

--- src/stagesOfTicTacToeGame/FourthStage.java
+++ src/stagesOfTicTacToeGame/FourthStage.java
@@ -7,6 +7,9 @@ public class FourthStage {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter cells: ");
         char[] cells = sc.next().toUpperCase().toCharArray();
+        System.out.println("---------");
+        char[][] newCells = {{cells[0], cells[1], cells[2]}, {cells[3], cells[4], cells[5]}, {cells[6], cells[7], cells[8]}};
+        System.out.print("| ");
 
     }
 }
