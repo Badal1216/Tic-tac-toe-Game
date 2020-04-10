7568ad5db1774ab218fb62ba67f21db38ad96c5c
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 13:32:21 +0530

    Create a Scanner class object to input cells by user

--- src/stagesOfTicTacToeGame/ThirdStage.java
+++ src/stagesOfTicTacToeGame/ThirdStage.java
@@ -1,7 +1,13 @@
 package stagesOfTicTacToeGame;
 
+import java.util.Scanner;
+
 public class ThirdStage {
     public static void main(String[] args) {
+        int count1 = 0;
+        int count2 = 0;
+        char index = ' ';
+        Scanner sc = new Scanner(System.in);
 
     }
 }
