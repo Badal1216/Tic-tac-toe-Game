7d835d7ff6884c96c00602bd9debb80b0078aca8
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-04-10 13:37:50 +0530

    Initialize the 2-D Array of Character type name as newCells

--- src/stagesOfTicTacToeGame/ThirdStage.java
+++ src/stagesOfTicTacToeGame/ThirdStage.java
@@ -10,6 +10,8 @@ public class ThirdStage {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter cells: ");
         char[] cells = sc.next().toUpperCase().toCharArray();
+        System.out.println("---------");
+        char[][] newCells = {{cells[0], cells[1], cells[2]}, {cells[3], cells[4], cells[5]}, {cells[6], cells[7], cells[8]}};
 
     }
 }
